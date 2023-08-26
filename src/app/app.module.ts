e27c13ef0c15c804a92bf4b428a412ddce652784
mdtuhinislam <mdtuhinislam@gmail.com>
2023-08-26 18:43:47 +0600

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      CreateStudentComponent
+   ],
   imports: [
     BrowserModule
   ],
